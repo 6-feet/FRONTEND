6d5d929460232f4da5f99825ea265d059c60a7ed
Виген Мовсисян <venmovs@MacBook-Pro-Vigen.local>
2022-05-21 11:39:58 +0300

    added airbnb lints

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,13 @@ import { AppProps } from 'next/app';
 import React from 'react';
 import Head from 'next/head';
 
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return (
-    <>
-      <Head>
-        <title>NextJS App From Scratch</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
-};
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
+  <>
+    <Head>
+      <title>NextJS App From Scratch</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 
 export default MyApp;
