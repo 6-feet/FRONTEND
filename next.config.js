fab678ba29ed6d5151b72cb3c834f774fdcab2b7
Виген Мовсисян <venmovs@MacBook-Pro-Vigen.local>
2022-05-21 10:28:32 +0300

    rebaised master in main

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+    reactStrictMode: true,
+    webpack(config, options) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: { and: [/\.(ts)x?$/] },
+            use: [
+                {
+                    loader: "@svgr/webpack",
+                    options: {
+                        svgoConfig: { plugins: [{ removeViewBox: false }] },
+                    },
+                },
+            ],
+        });
+
+        return config;
+    }
+}
+
+module.exports = nextConfig;
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+const Home = (): JSX.Element => {
+  return <div>Hello, NextJS!</div>;
+};
+
+export default Home;
